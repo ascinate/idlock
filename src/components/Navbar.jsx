762f5e3a7fbf50b415127650a11bf4f5004ae8e5
samrat <samrat.subratadas@gmail.com>
2025-06-10 00:09:09 +0530

    file news

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,6 +43,9 @@ function Navbar() {
 
 
                         <button  onClick={toggleMenu}  className="text-gray-800 dark:text-white focus:outline-none text-2xl md:hidden">
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                            </svg>
                         </button>
 
                         <div className="md:block md:w-auto" id="navbar-default">
