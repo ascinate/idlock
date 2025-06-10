f57f622bdd12043aabbb608d552e6791e5b8bf88
subrata <subrata@ascinatetech.com>
2025-06-10 12:52:52 +0530

    this page done 10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,13 +36,13 @@ function Navbar() {
 
             <nav className="bg-white float-left border-gray-200 dark:bg-gray-900 w-full">
                 <div className='container mx-auto'>
-                    <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+                    <div className="max-w-screen-xl moible-full flex flex-wrap items-center mx-auto p-4">
                         <NavLink to='/' className="flex loggos items-center space-x-3 rtl:space-x-reverse">
                             <img src='/mb-logo.png' className="logo-ps" alt='logo' />
                         </NavLink>
 
 
-                        <button  onClick={toggleMenu}  className="text-gray-800 dark:text-white focus:outline-none text-2xl md:hidden">
+                        <button  onClick={toggleMenu}  className="border-round-bar text-gray-800 dark:text-white focus:outline-none text-2xl md:hidden">
                             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                             </svg>
@@ -63,12 +63,14 @@ function Navbar() {
 
 
 
-                        <div className={`fixed top-0 left-0 w-64 h-full bg-white dark:bg-gray-900 shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
+                        <div className={`fixed top-0 offcanv left-0 w-64 h-full bg-white dark:bg-gray-900 shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
                                   isOpen ? 'translate-x-0' : '-translate-x-full'
                                 }`} >
                                 <div className="p-4 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
-                                  <h2 className="text-xl font-bold text-gray-900 dark:text-white">Menu</h2>
-                                  <button onClick={toggleMenu} className="text-gray-700 dark:text-white text-2xl">&times;</button>
+                                  <NavLink to="/" className="text-xl font-bold text-gray-900 dark:text-white">
+                                       <img src='/mobile-logo.png' alt='logo' />
+                                  </NavLink>
+                                  <button onClick={toggleMenu} className="clones-btn text-gray-700 dark:text-white text-2xl">&times;</button>
                                 </div>
                                 <ul className="mt-4">
                                   {navItems.map((item, index) => (
